77644f63b0d5964659edd0e30c62ca6f6f2a2bcc
cdufour <opusidea@gmail.com>
2021-10-25 14:32:29 +0000

    backup step 3

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,3 +1,4 @@
-DBNAME=tp1 # nom de la base de données à sauvegarder
+DBNAME  = tp1 # nom de la base de données à sauvegarder
+TIME    = $(date '+%Y-%m-%d_%H-%M-%S')
 
-mysqldump $DBNAME -uroot -p$MYSQL_ROOT_PASSWORD > dump/$DBNAME.sql
+mysqldump $DBNAME -uroot -p$MYSQL_ROOT_PASSWORD > dump/$DBNAME.$TIME.sql

6cdc3b2c01d408c5ce4621ab4270931b9f3610eb
cdufour <opusidea@gmail.com>
2021-10-26 07:29:17 +0000

    backup end

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,4 +1,6 @@
-DBNAME  = tp1 # nom de la base de données à sauvegarder
-TIME    = $(date '+%Y-%m-%d_%H-%M-%S')
+# Ce script sera exécuté dans le contexte d'un container docker mysql qui inclut mysqldump
+
+DBNAME=tp1 # nom de la base de données à sauvegarder
+TIME=$(date '+%Y-%m-%d_%H-%M-%S') # horodatage
 
 mysqldump $DBNAME -uroot -p$MYSQL_ROOT_PASSWORD > dump/$DBNAME.$TIME.sql
